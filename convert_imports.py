d2adf3f29c4cc41a7effd084c2f6c7186354e3f3
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2018-12-29 20:04:36 +0100

    Do not convert the current file.

--- convert_imports.py
+++ convert_imports.py
@@ -1,12 +1,12 @@
 import os
 
-strings_to_replace = [('import keras', 'from tensorflow.python import keras'),
+strings_to_replace = [('import keras', 'import tensorflow.python.keras'),
                       ('from keras.', 'from tensorflow.python.keras.'),
                       ('from keras import', 'from tensorflow.python.keras import')]
 
 
 def replace_imports(file_path):
-    if not file_path.endswith('.py'):
+    if not file_path.endswith('.py') or file_path.endswith('convert_imports.py'):
         return
     with open(file_path, 'r') as f:
         text = f.read()
